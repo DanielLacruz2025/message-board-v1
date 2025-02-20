c75c7b2e77890a6650cc34a73857a1fd44bd1992
Daniel <alejandrodelacruz1001@gmail.com>
2025-02-20 17:07:42 -0400

    str method added

---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Publication(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
